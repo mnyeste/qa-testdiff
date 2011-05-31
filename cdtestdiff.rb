514214a124cec89f4b7811b52066302e10733d86
mnyeste <misi@baxter-it.com>
2011-05-31 15:02:25 +0200

    Script finished

--- cdtestdiff.rb
+++ cdtestdiff.rb
@@ -8,7 +8,7 @@ def parseXML(folder)
   testcases = Array.new
 
   @log.info(" --- Processing #{folder}")
-  
+
   Dir.glob(File.join(folder,'*.xml')) do |filename|
 
     origcount = testcases.length
@@ -24,15 +24,28 @@ def parseXML(folder)
 
 end
 
+def logDifferences(minuend, subtrahend)
+  difference = minuend - subtrahend
+  @log.info("Difference: #{difference.length}")
+  @log.info("--- Testcase list begins ---")
+  difference.each {|e| @log.info(e)}
+  @log.info("--- Testcase list ends   ---")
+end
+
 @log = Logger.new(STDOUT)
-@log.level = Logger::DEBUG
+@log.formatter = proc { |severity, datetime, progname, msg| "#{severity} - #{datetime}: #{msg}\n" }
+@log.level = Logger::INFO
 
 eclipse  = parseXML("data/eclipse")
 surefire =  parseXML("data/surefire")
 
 @log.info(" --- Processing finished ---")
-@log.info("Eclipse: #{eclipse.length}")
+@log.info("Eclipse  : #{eclipse.length}")
+@log.info("Surefire : #{surefire.length}")
 
-print("Eclipse: ",eclipse.length)
+@log.info("--- Run only with Eclipse ---")
+logDifferences(eclipse, surefire)
+@log.info("--- Run only with Surefire ---")
+logDifferences(surefire, eclipse)
 
 @log.close
